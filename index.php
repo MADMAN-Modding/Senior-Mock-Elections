54628c4de9137571ed7ebfbd4fe421bf7e88c4e1
MADMAN-Modding <115199645+MADMAN-Modding@users.noreply.github.com>
2024-01-26 16:31:18 +0000

    Inital Files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Senior Mock Elections</title>
+        <link href="css/index.css" rel="stylesheet" type="text/css">
+        <script src="js/index.js"></script>
+    </head>
+    <body>
+        <h1>Have fun voting!</h1>
+        <?php
+            
+        ?>
+    </body>
+</html>
