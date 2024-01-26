087328770f94c1c1465a72f74fa506598dc184fb
MADMAN-Modding <115199645+MADMAN-Modding@users.noreply.github.com>
2024-01-26 16:46:03 +0000

    Some Testing with names

--- index.php
+++ index.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Senior Mock Elections</title>
-        <link href="css/index.css" rel="stylesheet" type="text/css">
-        <script src="js/index.js"></script>
-    </head>
-    <body>
-        <h1>Have fun voting!</h1>
-        <?php
-            
-        ?>
-    </body>
+
+<head>
+    <title>Senior Mock Elections</title>
+    <link href="css/index.css" rel="stylesheet" type="text/css">
+    <script src="js/index.js"></script>
+</head>
+
+<body>
+    <h1>Have fun voting!</h1>
+    <?php
+    $csvArray = array();
+
+    if (($handle = fopen("outputtedData.csv", "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $csvArray[] = $data;
+        }
+    }
+
+    fclose($handle);
+
+    for($i =0; $i <= $data.count($data(0)); $i++) {
+        echo $data(0,$i);
+    }
+
+    ?>
+</body>
+
 </html>
